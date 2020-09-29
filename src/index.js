02ca5bd2f0e8cf09f538528ccffaaec661f18786
N.Pantiukhova <NataliaPantiukhova@users.noreply.github.com>
2020-09-29 20:27:59 +0300

    init: resolve reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let str = `${n}`,
+    resultStr = '',
+    resultNumber = 0;
+  for (let i = 0; i < str.length; i++){
+      resultStr = `${str[i]}${resultStr}`;
+  };
+  resultNumber = parseInt(resultStr);
+  return resultNumber;
 }
